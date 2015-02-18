2bd7a94d571f53d56b615c59e5ae60fe8bd03a96
John Krah <john.krah@socrata.com>
2015-02-18 14:26:49 -0800

    add left middle right slash test cases

--- src/test/scala/com/socrata/sbtplugins/StringPathSpec.scala
+++ src/test/scala/com/socrata/sbtplugins/StringPathSpec.scala
@@ -4,13 +4,24 @@ import com.socrata.sbtplugins.StringPath._
 import org.scalatest.{ShouldMatchers, FunSuiteLike}
 
 class StringPathSpec extends FunSuiteLike with ShouldMatchers {
+  val expected =  "/this/is/a/test/"
+
   test("implicit conversion from string") {
-    val sp: StringPath = "/this/is/a/test/"
-    sp.path should equal("/this/is/a/test/")
+    val sp: StringPath = expected
+    sp.path should equal(expected)
   }
 
   test("path concatenation") {
-    val concat: String = "/this/" /  "/is/" / "/a/" / "/test/"
-    concat should equal("/this/is/a/test/")
+    val concat = "/this/" /  "/is/" / "/a/" / "/test/"
+    concat should equal(expected)
+
+    val concat1 = "this" / "is" / "a" / "test"
+    concat1 should equal(expected.substring(1, expected.length-1))
+
+    val concat2 = "this/" / "is/" / "a/" / "test/"
+    concat2 should equal(expected.substring(1, expected.length))
+
+    val concat3 = "/this" / "/is" / "/a" / "/test"
+    concat3 should equal(expected.substring(0, expected.length-1))
   }
 }
