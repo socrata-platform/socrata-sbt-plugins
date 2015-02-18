a47c5ef4ffd5e5d59d0c08e0121a2091633c22bb
John Krah <john.krah@socrata.com>
2015-02-18 11:35:42 -0800

    use File.pathSeparator/Char

--- src/main/scala/com/socrata/sbtplugins/StringPath.scala
+++ src/main/scala/com/socrata/sbtplugins/StringPath.scala
@@ -1,15 +1,16 @@
 package com.socrata.sbtplugins
 
+import java.io.File.{pathSeparator, pathSeparatorChar}
+
 /**
  * Convenience class to be able to write a String with '/' in code.
  */
 object StringPath {
-  val SLASH: String = "/"
   class StringPath(val path: String) {
     def / (part: String): String = path + // scalastyle:ignore method.name
-      (if (path.endsWith(SLASH)) "" else SLASH) +
-      (if (part.startsWith(SLASH)) part.substring(1) else part)
-    def asPath: String = SLASH + path.replace('.', '/')
+      (if (path.endsWith(pathSeparator)) "" else pathSeparator) +
+      (if (part.startsWith(pathSeparator)) part.substring(1) else part)
+    def asPath: String = pathSeparator + path.replace('.', pathSeparatorChar)
   }
   implicit def string2StringPath(path: String): StringPath = new StringPath(path)
 }
