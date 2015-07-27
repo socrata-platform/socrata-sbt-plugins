05e0d9756c8b8da8b04d5dc770fde88b361fa942
John Krah <john.krah@socrata.com>
2015-07-26 19:46:32 -0700

    findbugs test suite

---
+++ src/test/scala/com/socrata/sbtplugins/JavaFindBugsSpec.scala
@@ -0,0 +1,28 @@
+package com.socrata.sbtplugins.findbugs
+
+import com.socrata.sbtplugins.findbugs.JavaFindBugsPlugin._
+import org.scalatest.{FunSuiteLike, Matchers}
+import _root_.sbt.{Level => _, _}
+
+class JavaFindBugsSpec extends FunSuiteLike with Matchers {
+  val exampleFilename = "./src/test/resources/findbugs-result-example.xml"
+
+  test("triggers on all requirements") {
+    trigger should equal(AllRequirements)
+  }
+
+  test("depends on jvm plugin") {
+    requires should equal(plugins.JvmPlugin)
+  }
+
+  test("has project settings") {
+    projectSettings.isEmpty should equal(false)
+  }
+  
+  test("load example findbugs report xml") {
+    val example = file(exampleFilename)
+    val report = JavaFindBugsXml(Some(example)).report
+    report.bugs.map(_.summarize).length should be(32)
+    Option(report.summary.summarize) should be('defined)
+  }
+}
